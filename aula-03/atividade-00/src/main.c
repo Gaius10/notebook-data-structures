9cf332c9142bd8e879d8544e57d7a87cb1d9144e
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-08-12 09:14:35 -0300

    Fatorial

---
+++ aula-03/atividade-00/src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int fatorial(int x);
+
+int main() {
+    int x = 0;
+    if (scanf("%d", &x) != 1) {
+        printf("Erro ao ler entrada.\n");
+        return 1;
+    }
+
+    printf("Calculando fatorial de 5...\n");
+
+    int y = fatorial(x);
+    printf("Resultado final: %d", y);
+
+    return 0;
+}
+
+int fatorial(int x) {
+    printf("Entrando em fatorial(%d)\n", x);
+    if (x < 0) return 0;
+    if (x == 1 || x == 0) {
+        printf("Caso base atingido: fatorial(%d) = 1\n", x);
+        return 1;
+    }
+
+    int y = x * fatorial(x - 1);
+    printf("Retornando fatorial(%d) = %d\n", x, y);
+
+    return y;
+}
+
