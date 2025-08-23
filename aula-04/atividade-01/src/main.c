d756f3ee3c874f706342df442104035324a51caa
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-08-22 23:35:58 -0300

    Funcionando sem mensagens

--- aula-04/atividade-01/src/main.c
+++ aula-04/atividade-01/src/main.c
@@ -1,127 +1,99 @@
 #include <stdio.h>
 
-#define NUM_MAX_ITENS 1000
+#define ARR_MAX 1000
 
-typedef struct {
-    unsigned int len;
-    int items[NUM_MAX_ITENS];
-} List;
-
-List merge_sort(const List list);
-List split(
-    const List list,
-    const unsigned int start,
-    const unsigned int end
-);
-List merge(const List left, const List right);
-
-void print_list(List list);
+void merge_sort(int arr[], int start, int end);
+void merge(int arr[], int start, int mid, int end);
+void printl(int arr[], int start, int end);
 
 int main() {
-    List unordered_list;
+    int arr[ARR_MAX];
+    int n = 0;
+    int read;
 
-    for (int i = 0; i < NUM_MAX_ITENS; i++) {
-        unordered_list.items[i] = 0;
-    }
-
-    if (scanf("%d", &unordered_list.len) != 1) {
-        printf("Erro ao ler número de entradas.\n");
+    read = scanf("%d", &n);
+    if (read != 1) {
+        printf("Erro ao ler numero de entradas.\n");
         return 1;
     }
 
-    for (int i = 0; i < unordered_list.len; i++) {
-        if (scanf("%d", &unordered_list.items[i]) != 1) {
-            printf("Erro ao ler entrada %d.\n", i);
+    for (int i = 0; i < n; i++) {
+        read = scanf("%d", &arr[i]);
+        if (read != 1) {
+            printf("Erro ao ler entrada %d\n", i);
             return 1;
         }
     }
 
-    // printf("Lista desordenada: ");
-    // print_list(unordered_list);
+    printf("Lista desordenada: ");
+    printl(arr, 0, n);
 
-    const List ordered_list = merge_sort(unordered_list);
+    merge_sort(arr, 0, n);
 
-    // printf("\n");
-    // printf("Tamanho da lista: %d\n", unordered_list.len);
-    // printf("Tamanho da lista ordenada: %d\n", ordered_list.len);
-
-    // printf("Lista ordenada: ");
-    print_list(ordered_list);
+    printf("List ordenada: ");
+    printl(arr, 0, n);
 
     return 0;
 }
 
-List merge_sort(const List list) {
-    if (list.len == 1 || list.len == 0) {
-        return list;
-    }
+void merge_sort(int arr[], int start, int end) {
+    // Caso base
+    if (end - start <= 1) return;
 
-    List left = split(list, 0, list.len/2);
-    List right = split(list, list.len/2, list.len);
+    // Calcular meio
+    int mid = (start + end) / 2;
 
-    left = merge_sort(left);
-    right = merge_sort(right);
+    // Ordenar primeira metade
+    merge_sort(arr, start, mid);
 
-    List merged = merge(left, right);
+    // Ordenar segunda metade
+    merge_sort(arr, mid, end);
 
-    return merged;
+    // Mesclar
+    merge(arr, start, mid, end);
 }
 
-List split(
-    const List list,
-    const unsigned int start,
-    const unsigned int end
-) {
-    List new_list;
-    new_list.len = 0;
+void merge(int arr[], int start, int mid, int end) {
+    int left[ARR_MAX];
+    int right[ARR_MAX];
 
-    for (int i = start; i < end; i++) {
-        new_list.items[i-start] = list.items[i];
-        new_list.len += 1;
+    int l = 0;
+    int r = 0;
+
+    for (int i = start; i < mid; i++) {
+        left[i-start] = arr[i];
     }
 
-    return new_list;
-}
+    for (int i = mid; i < end; i++) {
+        right[i-mid] = arr[i];
+    }
 
-List merge(const List left, const List right) {
-    List new_list;
-    new_list.len = 0;
-
-    unsigned int aux_l = 0;
-    unsigned int aux_r = 0;
-
-    while (aux_l < left.len && aux_r < right.len) {
-        if (left.items[aux_l] > right.items[aux_r]) {
-            new_list.items[new_list.len] = right.items[aux_r];
-            aux_r += 1;
-            new_list.len += 1;
-            continue;
+    while (l < mid - start && r < end - mid) {
+        if (left[l] < right[r]) {
+            arr[start + r + l] = left[l];
+            l++;
+        } else {
+            arr[start + r + l] = right[r];
+            r++;
         }
-
-        new_list.items[new_list.len] = left.items[aux_l];
-        aux_l += 1;
-        new_list.len += 1;
     }
 
-    while (aux_l < left.len) {
-        new_list.items[new_list.len] = left.items[aux_l];
-        new_list.len += 1;
-        aux_l += 1;
+    while (l < mid - start) {
+        arr[start + r + l] = left[l];
+        l++;
     }
 
-    while (aux_r < right.len) {
-        new_list.items[new_list.len] = right.items[aux_r];
-        new_list.len += 1;
-        aux_r += 1;
+    while (r < end - mid) {
+        arr[start + r + l] = right[r];
+        r++;
     }
-
-    return new_list;
 }
 
-void print_list(List list) {
-    for (int i = 0; i < list.len; i++) {
-        printf("%d ", list.items[i]);
+void printl(int arr[], int start, int end) {
+    for (int i = start; i < end; i++) {
+        printf("%d ", arr[i]);
     }
+
     printf("\n");
 }
 
