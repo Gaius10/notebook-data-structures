acfe2a58db3646c9144b462d77de02aa2399d4f0
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-09-09 12:22:28 -0300

    Melhora interface

--- trabalho-01/analyzer/main.c
+++ trabalho-01/analyzer/main.c
@@ -10,25 +10,50 @@
 void show_menu(char* filename);
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if (argc != 3) {
         show_menu(argv[0]);
         return 1;
     }
 
     int op = *argv[1] - '0';
 
+    unsigned sample_quantities[4] = [10, 100, 1000, 5000];
+    unsigned sample_quantities_len = 4;
+
+    FILE* output_file = fopen(argv[2], 'w');
+    if (output_file == NULL) {
+        printf("Erro ao abrir arquivo de saída.\n");
+        return 1;
+    }
+
     switch (op) {
         case OP_INVERT:
-            analysis_invert();
+            analysis_invert(
+                sample_quantities,
+                sample_quantities_len,
+                output_file
+            );
             break;
         case OP_SEQ_SEARCH:
-            analysis_ss();
+            analysis_ss(
+                sample_quantities,
+                sample_quantities_len,
+                output_file
+            );
             break;
         case OP_BIN_SEARCH:
-            analysis_bs();
+            analysis_bs(
+                sample_quantities,
+                sample_quantities_len,
+                output_file
+            );
             break;
         case OP_REC_BIN_SEARCH:
-            analysis_rbs();
+            analysis_rbs(
+                sample_quantities,
+                sample_quantities_len,
+                output_file
+            );
             break;
         default:
             printf("Invalid option '%d'\n\n", op);
@@ -40,8 +65,8 @@ int main(int argc, char* argv[]) {
 }
 
 void show_menu(char* filename) {
-    printf("Usage: %s <option>\n", filename);
-    printf("Where <option> can be:\n");
+    printf("Usage: %s <algorithm> <output_filepath>\n", filename);
+    printf("Where <algorithm> can be:\n");
     printf("\t1. invert\n");
     printf("\t2. sequential search\n");
     printf("\t3. binary hearch\n");
--- trabalho-01/lib/analysis.h
+++ trabalho-01/lib/analysis.h
@@ -1,10 +1,29 @@
 #ifndef ANALYSIS_H
 #define ANALYSIS_H
 
-void analysis_invert();
-void analysis_ss();
-void analysis_bs();
-void analysis_rbs();
+void analysis_invert(
+    unsigned sample_quantities[],
+    unsigned sample_quantities_len,
+    FILE* output
+);
+
+void analysis_ss(
+    unsigned sample_quantities[],
+    unsigned sample_quantities_len,
+    FILE* output
+);
+
+void analysis_bs(
+    unsigned sample_quantities[],
+    unsigned sample_quantities_len,
+    FILE* output
+);
+
+void analysis_rbs(
+    unsigned sample_quantities[],
+    unsigned sample_quantities_len,
+    FILE* output
+);
 
 #endif
 
