15349e59867a51e37c49b1d0f2994e8ed93d9719
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-09-11 10:51:01 -0300

    Analysis of invert function

--- trabalho-01/lib/algorithms.h
+++ trabalho-01/lib/algorithms.h
@@ -2,10 +2,10 @@
 #ifndef ACTIONS_H
 #define ACTIONS_H
 
-void invert(int arr[], unsigned int arr_len);
-int sequential_search(int search, int arr[], unsigned int arr_len);
-int binary_search(int search, int arr[], unsigned int arr_len);
-int recursive_binary_search(int search, int arr[], unsigned int arr_len);
+void invert(int arr[], unsigned arr_len);
+int sequential_search(int search, int arr[], unsigned arr_len);
+int binary_search(int search, int arr[], unsigned arr_len);
+int recursive_binary_search(int search, int arr[], unsigned arr_len);
 
 #endif
 
